68a1a3821b5033eacf8798ab0943d4bc349462c8
Hiroshi Hosoda <hosoda-hiroshi@dmm.com>
2022-05-20 19:59:07 +0900

    Initial Commit

---
+++ Shared/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  Shared
+//
+//  Created by hosoda-hiroshi on 2022/05/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
